55bf239fae398564cac73329a59f89ad795736c8
Victoria Li <VLi@lcs.on.ca>
2021-01-23 16:19:12 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Li, Muchen on 2021/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
