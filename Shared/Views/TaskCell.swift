9294d2ebb192c37fed9e25361ecb10b10d7a331b
Victoria Li <VLi@lcs.on.ca>
2021-01-23 17:31:16 -0500

    computing 3 different text colors

--- Shared/Views/TaskCell.swift
+++ Shared/Views/TaskCell.swift
@@ -11,6 +11,17 @@ struct TaskCell: View {
     
     @ObservedObject var task: Task
     
+    var taskColor: Color {
+        switch task.priority {
+        case .high:
+            return Color.red
+        case .medium:
+            return Color.blue
+        case .low:
+            return Color.primary
+        }
+    }
+    
     var body: some View {
         HStack {
             Image(systemName: task.completed ? "checkmark.circle.fill" : "circle")
@@ -22,6 +33,7 @@ struct TaskCell: View {
             
             Text(task.description)
         }
+        .foregroundColor(self.taskColor)
     }
 }
 
